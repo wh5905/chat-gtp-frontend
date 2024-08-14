97dc8a634fe1e8d7ca5fdb908095b48a9b56a424
jung won hyeong <jeongwon264@gmail.com>
2024-08-14 14:26:31 +0900

    feat: [정원형] 서버로부터 주식 종목들의 정보를 받아 페이지에서 보여줍니다.  [PROJ-FRONT-17]

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,6 +3,7 @@ import authenticationModule from '@/authentication/store/authenticationModule'
 import boardModule from '@/board/store/boardModule'
 import GoogleAuthenticationModule from "@/googleAuthentication/store/GoogleAuthenticationModule"
 import NaverAuthenticationModule from '@/naverAuthentication/store/NaverAuthenticationModule'
+import StockModule from '@/stock/store/stockModule'
 import { createStore } from 'vuex'
 
 export default createStore({
@@ -20,5 +21,7 @@ export default createStore({
     authenticationModule,
     GoogleAuthenticationModule,
     NaverAuthenticationModule,
+    stock: StockModule,
+
   }
 })
