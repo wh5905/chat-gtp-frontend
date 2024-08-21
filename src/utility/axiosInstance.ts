67533b6c2416373065a1468635087f83f60aad0c
younghyen7956 <155882166+younghyen7956@users.noreply.github.com>
2024-08-21 13:59:50 +0900

    Revert "fix: [정원형] typescript 오류 해결 [PROJ-FRONT-17]"

--- src/utility/axiosInstance.ts
+++ src/utility/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance } from "axios"
 
 const djangoAxiosInst: AxiosInstance = axios.create({
     baseURL: env.api.MAIN_API_URL,
-    timeout: 10000,
+    timeout: 2500,
 })
 
 export default { djangoAxiosInst }
