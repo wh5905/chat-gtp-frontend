32671942818c2be9cd8af9e187abb69ba50e3931
jung won hyeong <jeongwon264@gmail.com>
2024-09-09 10:09:23 +0900

    feat: [정원형] 사용자가 주문한 주식 목록을 보여줍니다. [PROJ-FRONT-25]

--- src/stock/router/StockRoutes.ts
+++ src/stock/router/StockRoutes.ts
@@ -1,5 +1,6 @@
 import StockListPage from "@/stock/pages/list/StockList.vue";
 import StockDetailPage from "@/stock/pages/detail/StockDetail.vue";
+import TradingPage from "@/stock/pages/trading/Trading.vue";
 
 const StockRoutes = [
     {
@@ -11,7 +12,12 @@ const StockRoutes = [
         path: '/stock/:ticker',
         name: 'stock',
         component: StockDetailPage
-      }
+      },
+      {
+        path: '/stock/trade',
+        name: 'stock_trade',
+        component: TradingPage
+      },
 ];
 
 
